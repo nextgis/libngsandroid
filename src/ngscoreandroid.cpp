b994c7f9549bc037e434d446c23aa3829d5b1293
NikitaFeodonit <nikitafeodonit@yandex.com>
2016-07-08 03:21:08 +0300

    CMake files. Android specific. OpenGL renderer

---
+++ src/ngscoreandroid.cpp
@@ -0,0 +1,134 @@
+#include <jni.h>
+#include <android/bitmap.h>
+
+#include <iostream>
+
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+
+
+// http://stackoverflow.com/a/22693766
+extern "C" jlong Java_com_nextgis_store_NgsCoreAndroid_lockBitmapPixels(
+        JNIEnv* env,
+        jclass type,
+        jobject bitmap)
+{
+    void* pPixels;
+    int ret;
+
+    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pPixels)) < 0) {
+        std::cerr << "Error - AndroidBitmap_lockPixels() Failed! error: " << ret << std::endl;
+        return 0;
+    }
+
+    return reinterpret_cast<jlong> (pPixels);
+}
+
+
+extern "C" void Java_com_nextgis_store_NgsCoreAndroid_unlockBitmapPixels(
+        JNIEnv* env,
+        jclass type,
+        jobject bitmap)
+{
+    AndroidBitmap_unlockPixels(env, bitmap);
+}
+
+
+bool renderer(
+        void* imagePtr,
+        uint32_t imageWidth,
+        uint32_t imageHeight)
+{
+//    BOOST_LOG_SEV(GmLog::log, debug) << "testPath: " << testPath;
+
+    // http://stackoverflow.com/q/28817777
+    // http://stackoverflow.com/a/27092070
+    // TODO: work with OpenGL errors
+    // Step 1 - Get the default display.
+    EGLDisplay eglDisplay = eglGetDisplay((EGLNativeDisplayType) 0);
+
+    // Step 2 - Initialize EGL.
+    eglInitialize(eglDisplay, 0, 0);
+
+    // Step 3 - Make OpenGL ES the current API.
+    eglBindAPI(EGL_OPENGL_API);
+
+    // Step 4 - Specify the required configuration attributes.
+    EGLint pi32ConfigAttribs[9];
+    pi32ConfigAttribs[0] = EGL_COLOR_BUFFER_TYPE;
+    pi32ConfigAttribs[1] = EGL_RGB_BUFFER;
+    pi32ConfigAttribs[2] = EGL_LEVEL;
+    pi32ConfigAttribs[3] = 0;
+    pi32ConfigAttribs[4] = EGL_RENDERABLE_TYPE;
+    pi32ConfigAttribs[5] = EGL_OPENGL_ES2_BIT;
+    pi32ConfigAttribs[6] = EGL_SURFACE_TYPE;
+    pi32ConfigAttribs[7] = EGL_PBUFFER_BIT;
+    pi32ConfigAttribs[8] = EGL_NONE;
+
+    // Step 5 - Find a config that matches all requirements.
+    int iConfigs;
+    EGLConfig eglConfig;
+    eglChooseConfig(eglDisplay, pi32ConfigAttribs, &eglConfig, 1, &iConfigs);
+//    BOOST_LOG_SEV(GmLog::log, debug) << "egl error" << eglGetError();
+
+    if (iConfigs != 1) {
+//        throw GmCoreErrEx() << GmCoreErrInfo("Error: eglChooseConfig(): config not found.");
+        return false;
+    }
+
+    EGLint pbufferAttribs[5];
+    pbufferAttribs[0] = EGL_WIDTH;
+    pbufferAttribs[1] = imageWidth;
+    pbufferAttribs[2] = EGL_HEIGHT;
+    pbufferAttribs[3] = imageHeight;
+    pbufferAttribs[4] = EGL_NONE;
+
+    // Step 6 - Create a surface to draw to.
+    EGLSurface eglSurface;
+    eglSurface = eglCreatePbufferSurface(eglDisplay, eglConfig, pbufferAttribs);
+//    BOOST_LOG_SEV(GmLog::log, debug) << "egl error" << eglGetError();
+
+    if (eglSurface == EGL_NO_SURFACE) {
+//        BOOST_LOG_SEV(GmLog::log, debug) << "surface issue";
+        return false;
+    }
+
+    // Step 7 - Create a context.
+    EGLContext eglContext;
+    eglContext = eglCreateContext(eglDisplay, eglConfig, NULL, NULL);
+//    BOOST_LOG_SEV(GmLog::log, debug) << "egl error" << eglGetError();
+
+    if (eglContext == EGL_NO_CONTEXT) {
+//        BOOST_LOG_SEV(GmLog::log, debug) << "context issue";
+        return false;
+    }
+
+    // Step 8 - Bind the context to the current thread
+    bool result = eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext);
+
+    if (!result) {
+//        BOOST_LOG_SEV(GmLog::log, debug) << "make current error" << eglGetError();
+        return false;
+    }
+//    BOOST_LOG_SEV(GmLog::log, debug) << "egl error" << eglGetError();
+
+    glClearColor(0.0, 1.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    eglSwapBuffers(eglDisplay, eglSurface);
+    glReadPixels(0, 0, imageWidth, imageHeight, GL_RGBA, GL_UNSIGNED_BYTE, imagePtr);
+
+    // set the reply data
+    return true;
+}
+
+
+extern "C" jboolean Java_com_nextgis_store_NgsCoreAndroid_fillImage(
+        JNIEnv* env,
+        jclass type,
+        jlong imagePointer,
+        jint imageWidth,
+        jint imageHeight)
+{
+    void* imagePtr = reinterpret_cast<void*> (imagePointer);
+    return renderer(imagePtr, imageWidth, imageHeight);
+}
